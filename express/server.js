eab26b34154a196f2d44b07f68c88472d371bb81
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-08-24 10:01:51 -0500

    add pdb logic. use let

--- express/server.js
+++ express/server.js
@@ -1,13 +1,13 @@
-var express = require("express"); 
-var bodyParser = require('body-parser');
-var path = require('path'); 
-var expressValidator = require('express-validator');  
-var upload = require("express-fileupload");
-var fs = require('fs');
-var cors = require('cors');
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+const upload = require("express-fileupload");
+const fs = require('fs');
+const cors = require('cors');
 
 
-var app = express();
+const app = express();
 app.use(cors());
 
 
@@ -20,9 +20,8 @@ app.use(upload());
 // express validator middleware 
  app.use(expressValidator({
  	errorFormater: function (param, msg, value) {
- 		var namespace = param.split('.')
- 		, root = namespace.shift()
- 		, formParam = root; 
+ 		let namespace = param.split('.');
+ 		let formParam = namespace.shift();
 
  		while (namespace.length) {
  			formParam += '[' + namespace.shift() + ']'; 
@@ -49,37 +48,52 @@ app.get('/', function (req, res) {
 
 // handle uploads 
 app.post('/upload', function(req, res) {
-	
-    // ensure files are uploaded
-	if (!req.files) {
-		return res.status(400).send('No files were upload.'); 
-	}
-
-	var pdb = req.files.pdbFile;
-
-    // create a randomly named folder by appending a random number to the upload/ directory
-	var epoch = (new Date).getTime().toString();
-	var dir = '/home/monster_uploads/upload/' + epoch;
-
-    // while a folder of the same name exists, keep getting random numbers
-	while (fs.existsSync(dir)) {
-		dir = dir + "_new";
-	}
-
-    // create the directory upload the file to it
-	fs.mkdirSync(dir);
-	fs.chmod(dir, 0o777);
-	var file = dir + '/' + pdb.name;
-	var url_path = 'http://monster.northwestern.edu/files/upload/' + epoch + '/' + pdb.name;
-
-	pdb.mv(file, function (err) {
-		if (err) {
-			return res.status(500).send(err); 
+    let url_path;
+    let file;
+    let pdbID = req.body.pdbId;
+    if (pdbID) {
+    	if (!RegExp('[A-z0-9]{4}').test(pdbID)) {
+    		return res.status(400).send(`${pdbID} does not appear to be a valid PDB ID`);
 		}
-		console.log('file uploaded: ' + file);
-		// parse the file
+    	file = `/home/pdb-mirror/data/structures/all/pdb/pdb${pdbID}.ent.gz`;
+		if (!fs.existsSync(file)){
+    		return res.status(400).send(`${pdbID} is not present in the local PDB cache. 
+    		Please use the file upload`);
+		}
+		url_path = `http://monster.northwestern.edu/files/pdb/pdb${pdbID}.ent.gz`;
 		parse(file, url_path, res);
-	});
+
+    } else {
+        // ensure files are uploaded
+        if (!req.files) {
+            return res.status(400).send('No files were upload.');
+        }
+        let pdb = req.files.pdbFile;
+
+        // create a randomly named folder by appending a random number to the upload/ directory
+        let epoch = (new Date).getTime().toString();
+        let dir = '/home/monster_uploads/upload/' + epoch;
+
+        // while a folder of the same name exists, keep getting random numbers
+        while (fs.existsSync(dir)) {
+            dir = dir + "_new";
+        }
+
+        // create the directory upload the file to it
+        fs.mkdirSync(dir);
+        fs.chmod(dir, 0o777);
+        file = dir + '/' + pdb.name;
+        url_path = 'http://monster.northwestern.edu/files/upload/' + epoch + '/' + pdb.name;
+
+        pdb.mv(file, function (err) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            console.log('file uploaded: ' + file);
+            // parse the file
+            parse(file, url_path, res);
+        });
+    }
 });
 
 app.listen(9000, function() {
@@ -89,17 +103,17 @@ app.listen(9000, function() {
 
 // parses the uploaded pdb file. takes the folder as input 
 function parse(file, url_path, res) {
-	var chains = [],
-		currID = false,
-		startRes = false,
-		endRes = false;
-	fs.readFile(file, function(err, data) {
+    let chains = [];
+    let currID = false;
+    let startRes = false;
+    let endRes = false;
+    fs.readFile(file, function(err, data) {
     	//if(err) throw err;
 		// each index of the array holds a line of the pdb file
-			var line,
-    		lines = data.toString().split("\n");
+        let line;
+        let lines = data.toString().split("\n");
 
-		for (line of lines){
+        for (line of lines){
 			//console.log(line);
 			if ("ATOM" === line.slice(0,4)){
 				if (!currID) {
