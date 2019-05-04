da10d3033a7f8d93c5536f04f9f381265d6374dd
Joey Yang <zeyuyang2022@u.northwestern.edu>
2019-05-04 14:33:57 -0500

    Updated server.js

--- express/server.js
+++ express/server.js
@@ -1,149 +1,71 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
-const https = require('https');
-const convert = require('xml-js');
 const expressValidator = require('express-validator');
 const upload = require("express-fileupload");
 const fs = require('fs');
 const cors = require('cors');
-
-// Get environment variables
-const PORT = process.env.PORT || 9001;
-const SSL_KEY = process.env.SSL_KEY;
-const SSL_CERT = process.env.SSL_CERT;
-const UPLOAD_DIR = process.env.UPLOAD_DIR || '/home/monster_uploads/upload';
-const UPLOAD_URL = process.env.UPLOAD_URL || 'http://monster.northwestern.edu/files/upload';
-const JOBS_DIR = process.env.JOBS_DIR || '/home/monster_uploads/jobs';
-const DL_URL = process.env.DL_URL || 'http://monster.northwestern.edu/jobs';
 
 
 const app = express();
 app.use(cors());
 
 
-// order important, must put middleware before route handler 
-// body parser middleware 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: false})); 
-app.use(upload()); 
+// order important, must put middleware before route handler
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(upload());
 
-// express validator middleware 
+// express validator middleware
  app.use(expressValidator({
  	errorFormater: function (param, msg, value) {
  		let namespace = param.split('.');
  		let formParam = namespace.shift();
 
  		while (namespace.length) {
- 			formParam += '[' + namespace.shift() + ']'; 
+ 			formParam += '[' + namespace.shift() + ']';
  		}
  		return {
  			param : formParam,
  			msg : msg,
  			value : value
- 		}; 
+ 		};
  	}
- })); 
+ }));
 
-// view engine 
-app.set('view engine', 'pug'); 
-app.set('views', path.join(__dirname, 'views')); 
+// view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // route handler
 app.get('/', function (req, res) {
 	res.render('index', {
-		chains : [], 
+		chains : [],
         pairs : []
-	}); 
-});
-
-app.get('/results/:job_id', function(req, res) {
-	const jobs_dir = `${JOBS_DIR}/${req.params.job_id}`;
-	const dl_url = `${DL_URL}/${req.params.job_id}`;
-	let response = {job_id: req.params.job_id};
-	response.models = {};
-	fs.readdirSync(jobs_dir).forEach(item => {
-		if (fs.lstatSync(`${jobs_dir}/${item}`).isDirectory()) {
-			let chains = [];
-			response.models[item] = {};
-			fs.readdirSync(`${jobs_dir}/${item}`).forEach(file => {
-				// Assuming single character chain IDs
-				if (file.match(/..bonds\.xml/)) {chains.push(file.slice(0,2))}
-			});
-			chains.forEach(chain => {
-				const xml = fs.readFileSync(`${jobs_dir}/${item}/${chain}bonds.xml`, 'utf8');
-				const parsed_xml = convert.xml2json(xml, {compact: true});
-				response.models[item][chain] = {
-					Results: {
-						XML: `${dl_url}/${item}/${chain}bonds.xml`,
-						TXT: `${dl_url}/${item}/${chain}bonds.txt`
-                    },
-					Logs: {
-						MSMS: `${dl_url}/${item}/${chain}msms.log`,
-						HBPlus: `${dl_url}/${item}/${chain}hb.log`
-					},
-					PDB: {
-						PDB: `${dl_url}/${item}/${chain}.pdb`
-					},
-					parsed_bonds: parsed_xml
-				}
-			})
-		} else if (item.match(/consensus\.xml/)) {
-			const chain = item.slice(0,2);
-			if (!response.models.Consensus) {
-                response.models.Consensus = {};
-            }
-            const xml = fs.readFileSync(`${jobs_dir}/${item}`, 'utf8');
-			const parsed_xml = convert.xml2json(xml, {compact: true});
-            response.models.Consensus[chain] = {
-				Results: {
-						XML: `${dl_url}/${chain}consensus.xml`,
-						TXT: `${dl_url}/${chain}consensus.txt`
-                    },
-				parsed_bonds: parsed_xml
-			}
-
-		}
 	});
-	res.send(response);
 });
 
-// handle uploads 
+// handle uploads
 app.post('/upload', function(req, res) {
     let url_path;
     let file;
-    // create a randomly named folder by appending a random number to the upload/ directory
-	let epoch = (new Date).getTime().toString();
-	let dir = UPLOAD_DIR + '/' + epoch;
-
-	// while a folder of the same name exists, keep getting random numbers
-	while (fs.existsSync(dir)) {
-		dir = dir + "_new";
-	}
-
-	// create the directory upload the file to it
-	fs.mkdirSync(dir);
-	fs.chmodSync(dir, 0o777);
+    let email = req.body.email;
     if (req.body.pdbId) {
         let pdbID = req.body.pdbId.toLowerCase();
     	if (!RegExp('^[a-z0-9]{4}$').test(pdbID)) {
     		console.log('Bad PDB: ' + pdbID);
     		return res.status(400).send(`${pdbID} does not appear to be a valid PDB ID`);
 		}
-		const web_address = `https://files.rcsb.org/download/${pdbID}.pdb`;
-    	const file_path = `${dir}/${pdbID}.pdb`;
-    	file = fs.createWriteStream(file_path);
-		https.get(web_address, function(response) {
-			if (response.statusCode !== 200){
-				console.log('Bad PDB Download: ' + pdbID);
-				return res.status(400).send(`Failed to fetch a PDB file for ${pdbID} from RCSB`)
-			}
-			response.pipe(file).on('finish', function () {
-				console.log('Download PDB from RCSB: ' + file_path);
-				url_path = `${UPLOAD_URL}/${epoch}/${pdbID}.pdb`;
-				parse(file_path, url_path, res);
-			});
-		});
+    	file = `/home/pdb-mirror/data/structures/all/pdb/pdb${pdbID}.ent.gz`;
+		if (!fs.existsSync(file)){
+			console.log('Missing PDB: ' + pdbID);
+    		return res.status(400).send(`${pdbID} is not present in the local PDB cache.
+    		Please use the file upload`);
+		}
+		console.log('Using PDB cache: ' + file);
+		url_path = `http://monster.northwestern.edu/files/pdb/pdb${pdbID}.ent.gz`;
+		parse(file, url_path, res);
 
     } else {
         // ensure files are uploaded
@@ -152,9 +74,24 @@ app.post('/upload', function(req, res) {
         }
         let pdb = req.files.pdbFile;
 
-        file = dir + '/' + pdb.name;
-        url_path = `${UPLOAD_URL}/${epoch}/${pdb.name}`;
+        // create a randomly named folder by appending a random number to the upload/ directory
+        let epoch = (new Date).getTime().toString();
+        let dir = '../home/monster_uploads/upload/' + epoch;
 
+        // while a folder of the same name exists, keep getting random numbers
+        while (fs.existsSync(dir)) {
+            dir = dir + "_new";
+        }
+
+        // create the directory upload the file to it
+        fs.mkdirSync(dir);
+        fs.chmod(dir, 0o777, (err) => {
+            if (err) throw err;
+            console.log('Changed');
+        });
+        file = dir + '/' + pdb.name;
+
+        url_path = 'http://monster.northwestern.edu/files/upload/' + epoch + '/' + pdb.name;
         pdb.mv(file, function (err) {
             if (err) {
                 return res.status(500).send(err);
@@ -163,24 +100,16 @@ app.post('/upload', function(req, res) {
             // parse the file
             parse(file, url_path, res);
         });
+        makeXMLFile(file, url_path, email);
     }
 });
 
 app.listen(9000, function() {
-	console.log('HTTP server started on port 9000')
+	console.log('server started on port 9000')
 });
 
-if (SSL_CERT) {
-    https.createServer({
-        key: fs.readFileSync(SSL_KEY),
-        cert: fs.readFileSync(SSL_CERT)
-    }, app).listen(PORT, function () {
-        console.log('https app listening on port ' + PORT)
-    });
-}
 
-
-// parses the uploaded pdb file. takes the folder as input 
+// parses the uploaded pdb file. takes the folder as input
 function parse(file, url_path, res) {
     let chains = [];
     let currID = false;
@@ -224,4 +153,36 @@ function parse(file, url_path, res) {
 			"chains": chains,
 		});
 	});
+}
+
+function makeXMLFile(file, url_path, email){
+    let index = Math.random().toString(36).slice(-10);
+    let dirxml = './test/jobs/' + index;
+    fs.mkdirSync(dirxml);
+    filexml = dirxml + '/' + index + '.xml';
+    fs.writeFile(filexml, makeXML(file, url_path, index, email), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+function makeXML(file, url_path, index, email) {
+    let chain_pair = [];
+    let selected_pairs = [];
+    let txt = `<ChainPairs index='${index}' protons='false'><File>${url_path}</File><Email>${email}</Email><IP>Unknown</IP><OS>Unknown</OS>`;
+    for (let chain_pair of selected_pairs) {
+        txt += `<ChainPair index='${chain_pair}'>${getChainXML(chain_pair[0])}${getChainXML(chain_pair[1])}</ChainPair>`;
+    }
+    txt += '</ChainPairs>';
+    console.log(txt);
+    return txt;
+ }
+
+function getChainXML(id) {
+    let all_chains = [];
+    for (let chain of all_chains) {
+        if (chain.name === id) {
+            return `<Chain index='${chain.name}'><Start>${chain.start}</Start><End>${chain.end}</End><Natural>${chain.name}</Natural></Chain>`;
+        }
+    }
 }
