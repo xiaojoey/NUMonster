53c08819841effbbe351672b8f4fc928605009af
Joey Yang <zeyuyang2022@u.northwestern.edu>
2019-05-23 03:32:33 -0500

    more updates

--- express/perlbackend.sh
+++ express/perlbackend.sh
@@ -6,7 +6,7 @@ cd ../perl_backend
 echo 'executing shell script with arg:' $*
 
 #pipes the first argument to monster_web
-#echo $1 | ./monster_web
+echo $1 | ./monster_web
 
 echo 'current directory is:'
 pwd
