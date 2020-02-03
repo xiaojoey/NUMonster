899d5339be6afd3df2d128266dcf7bf34c8ad0f3
Sam Seaver <samseaver@gmail.com>
2020-02-03 10:22:26 -0600

    minor differences for development environment

--- express/server/index.js
+++ express/server/index.js
@@ -11,11 +11,6 @@ const cors = require('cors');
 const exec = require('child_process').exec;
 
 // Get environment variables
-<<<<<<< HEAD
-=======
-const PORT = process.env.PORT || 9001;
-
->>>>>>> perl_dev
 const UPLOAD_DIR = process.env.UPLOAD_DIR || '/home/monster_uploads/upload';
 const UPLOAD_URL = process.env.UPLOAD_URL || 'http://monster.northwestern.edu/files/upload';
 const JOBS_DIR = process.env.JOBS_DIR || '/home/monster_uploads/jobs';
@@ -146,7 +141,7 @@ app.post('/upload', function(req, res) {
 			}
 			response.pipe(file).on('finish', function () {
 				console.log('Download PDB from RCSB: ' + file_path);
-				url_path = `${UPLOAD_URL}/${epoch}/${pdbID}.pdb`;
+				url_path = `${epoch}/${pdbID}.pdb`;
 				parse(file_path, url_path, res);
 			});
 		});
@@ -159,7 +154,7 @@ app.post('/upload', function(req, res) {
         let pdb = req.files.pdbFile;
 
         file = dir + '/' + pdb.name;
-        url_path = `${UPLOAD_URL}/${epoch}/${pdb.name}`;
+        url_path = `${epoch}/${pdb.name}`;
 
         pdb.mv(file, function (err) {
             if (err) {
