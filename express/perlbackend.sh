5c77c743d429e2a4ce1086922ca416bbf6b6bee1
Sam Seaver <samseaver@gmail.com>
2020-11-12 12:03:21 -0600

    Fixing bugs with running in production

--- express/perlbackend.sh
+++ express/perlbackend.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#changes directory to the location of monster_web
-cd ../perl_backend
-
 echo 'executing shell script with arg:' $*
 
 #pipes the first argument to monster_web
-./monster_web $1
+su - monster -c "cd ~monster/NUMonster/perl_backend;./monster_web $1"
 
 # echo 'current directory is:'
 # pwd
